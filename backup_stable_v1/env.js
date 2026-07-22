e6316dce145bb41928f59611455e08fafe8e79ae
Sahil Faraz <189783482+sahilfaraz0@users.noreply.github.com>
2026-07-22 14:37:19 +0500

    Removed unused file

--- backup_stable_v1/env.js
+++
@@ -1,4 +0,0 @@
-window.ENV = window.ENV || {
-  SUPABASE_URL: 'https://ocaaggbwapmexaxptump.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYWFnZ2J3YXBtZXhheHB0dW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMjQxMzcsImV4cCI6MjA5OTcwMDEzN30.GKyxFO0zsdzdotfY757_mA4rUgmkJ9syonOH-nFOE8w'
-};
